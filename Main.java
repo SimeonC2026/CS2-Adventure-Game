bc5ddb0e10e8ee6bdbcbdbfe240cdd2855d44984
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-12-01 14:39:18 +0000

    Added directions

--- Main.java
+++ Main.java
@@ -1,6 +1,43 @@
-public class Main {
+import java.util.Scanner;
+ public class Main 
+ {
+    public static void main(String []args) 
+    {
+       System.out.println("Starting a new adventure...\n");
+       String command = ""; // stores the user's choices
+      
+       // STEP #1: Describe the current scenario
+       System.out.println("You are at the Columbus Circle entrance to Central Park.");
+       // STEP #2: Get user command 
+       command = getCommand("Do you go east (e) or south (s)?");
+       // STEP #3: Select a path based on the user's command
+       if (  command.equals("e") ) {
+         // NEXT BRANCH...
+         System.out.println("You reach the Carousel.");
+       }
+       else if ( command.equals("s") ) {
+         System.out.println("You see a wedding at the lake with the sailboats");
+       }
+       else {
+         System.out.println("GAME OVER! You got lost in the Time Warner center shopping for hours. Press RUN to replay.");
+       }
 
-   public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+
+    
+       
+    } // END OF MAIN METHOD
+    
+
+    /** Method to prompt & process input for the next command
+      * @param prompt to display to user, asks for specific input
+      * @return user's input command as a lowercase String
+    */
+    public static String getCommand(String prompt) 
+    {
+       System.out.println("\n▶︎▶︎▶︎ " + prompt);
+       Scanner scan = new Scanner(System.in);
+       String command = scan.nextLine().toLowerCase();
+       scan.close();
+       return command;
+    } // END OF METHOD
+ } // END OF CLASS
